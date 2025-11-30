99400f0d8151226b61f233e0bade9db3db7363dc
daniel <danttaz11@gmail.com>
2025-11-30 11:06:46 -0300

    tudo certo, primeiro commit

---
+++ src/main/java/com/example/Crud/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.Crud.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("test")
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String test(){
+        return "Test API --------------------";
+    }
+}
